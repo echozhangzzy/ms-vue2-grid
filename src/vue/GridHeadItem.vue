c544d0d2779362d80a46e9cae6bfb0520eba3b89
Bane.Shi <fengxiaotx@163.com>
2017-01-10 22:10:17 +0800

    [Bane.Shi]

---
+++ src/vue/GridHeadItem.vue
@@ -0,0 +1,44 @@
+<!--
+Created by Bane.Shi.
+Copyright MoenSun
+User: Bane.Shi
+Date: 2017/1/10
+Time: 15:25-->
+<template>
+  <div class="ms-grid-head-item" :style="customStyle">
+    <div>{{column.text}}</div>
+  </div>
+</template>
+<script>
+    export default {
+      props:{
+        column:{
+          type:Object
+        },
+        flexCount:{
+          type:Number
+        },
+        restWidth:{
+          type:Number
+        }
+      },
+      computed:{
+        customStyle:function(){
+          let me = this;
+          let style = {};
+          if(me.column.width){
+            Object.assign(style,{width:me.column.width+'px'});
+          }else {
+            let width = (me.column)*me.restWidth;
+            Object.assign(style,{width: +'px'});
+          }
+          return style;
+        }
+      },
+      components: {
+      }
+    }
+</script>
+<style>
+
+</style>
